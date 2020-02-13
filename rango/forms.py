f75da072274dd713e2facae126039711120cfcf8
Christos Stylianou <43452764+chrisstyl@users.noreply.github.com>
2020-02-13 18:54:41 +0000

    Chapter 9,1 error

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -32,7 +34,8 @@ class PageForm(forms.ModelForm):
         # we can either exclude the category field from the form,
         exclude = ('category',)
         # or specify the fields to include (don't include the category field).
-        #fields = ('title', 'url', 'views')
+        #fields = ('title', 'url', 'views')
+
 
 
     def clean(self):
@@ -47,3 +50,15 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
         return cleaned_data
 
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,6 +19,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
+LOGIN_URL = 'rango:login'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -96,6 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length':6,},
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
